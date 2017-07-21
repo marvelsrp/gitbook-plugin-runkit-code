363e311806cee922826fdee04c24f46bbae68168
Maksym Bugai <m.bugai@plarium.com>
2017-07-21 17:09:07 +0300

    0.0.16

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = {
   hooks: {
-    "page": function(page) {
+    page: function(page) {
       page.content += '<script src="https://embed.runkit.com" data-element-id="runkit-code"></script>';
       page.content = page.content.replace('class="lang-runkit"', 'class="lang-runkit" id="runkit-code"');
-
+      console.log('page.content', page.content);
       return page;
     }
-  }
+  },
+  blocks: {},
+  filters: {}
 };
